bcb472cc4f32070d7e95b8cd5ad306876e524465
techiebirb <tahsanh12345@gmail.com>
2025-01-29 13:32:18 -0500

    s

--- main.lua
+++ main.lua
@@ -5,23 +5,8 @@ local Window = OrionLib:MakeWindow(
         HidePremium=true,
         SaveConfig=true,
         ConfigFolder="Techiebirb's Scripts",
-        IntroEnabled=true,
-        IntroText="TBScripts"
+        IntroEnabled=false
     }
 )
 
-
---[[
-IntroEnabled = <bool> - Whether or not to show the intro animation.
-IntroText = <string> - Text to show in the intro animation.
-IntroIcon = <string> - URL to the image you want to use in the intro animation.
-Icon = <string> - URL to the image you want displayed on the window.
-CloseCallback = <function> - Function to execute when the window is closed.
-
-IntroIcon="https://avatars.githubusercontent.com/u/73718824",
-Icon="https://avatars.githubusercontent.com/u/73718824"
-
-]]--
-
-end
 OrionLib:Init()
