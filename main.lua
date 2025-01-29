c8d808f85ce7ac74ed9361922f4ee137058c5f1e
techiebirb <73718824+techiebirb@users.noreply.github.com>
2025-01-29 17:13:26 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,24 +1,23 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-    Name = "Techiebirb's Scripts",
-    Icon = "origami",
-    LoadingTitle = "Techiebirb's Scripts",
-    LoadingSubtitle = "by Sirius",
-    Theme = "Default",
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
-    ConfigurationSaving = {
-        Enabled = true,
-        FolderName = nil,
-        FileName = "TBScriptsConfig"
-    },
-    Discord = {
-        Enabled = false,
-        Invite = "noinvitelink",
-        RememberJoins = true
-    },
+   Name = "Techiebirb's Scripts",
+   Icon = "origami",
+   LoadingTitle = "Techiebirb's Scripts",
+   LoadingSubtitle = "by Sirius",
+   Theme = "Default",
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "TBScriptsConfig"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
    KeySystem = false,
- })
-
+})
 
 Rayfield:LoadConfiguration()
